1b4a2f4ea5a4379de2a07fdec762eeeb10205816
S2codes <25subhankar@gmail.com>
2022-02-05 02:16:51 +0530

    intial push

---
+++ assets/js/main.js
@@ -0,0 +1,63 @@
+/*===== SHOW NAVBAR  =====*/ 
+const showNavbar = (toggleId, navId, bodyId, headerId) =>{
+    const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId),
+    bodypd = document.getElementById(bodyId),
+    headerpd = document.getElementById(headerId)
+
+    // Validate that all variables exist
+    if(toggle && nav && bodypd && headerpd){
+        toggle.addEventListener('click', ()=>{
+            // show navbar
+            nav.classList.toggle('show')
+            // change icon
+            toggle.classList.toggle('bx-x')
+            // add padding to body
+            bodypd.classList.toggle('body-pd')
+            // add padding to header
+            headerpd.classList.toggle('body-pd')
+        })
+    }
+}
+
+showNavbar('header-toggle','nav-bar','body-pd','header')
+
+/*===== LINK ACTIVE  =====*/ 
+const linkColor = document.querySelectorAll('.nav__link')
+
+function colorLink(){
+    if(linkColor){
+        linkColor.forEach(l=> l.classList.remove('active'))
+        this.classList.add('active')
+    }
+}
+linkColor.forEach(l=> l.addEventListener('click', colorLink))
+
+// ====== chart ====== //
+
+const data = {
+    labels: [
+        'Product 1',
+        'Product 2',
+        'Product 3'
+    ],
+    datasets: [{
+        label: 'My First Dataset',
+        data: [250, 100, 50],
+        backgroundColor: [
+            'rgb(255, 99, 132)',
+            'rgb(54, 162, 235)',
+            'rgb(255, 205, 86)'
+        ],
+        hoverOffset: 4
+    }]
+};
+
+const config = {
+    type: 'pie',
+    data: data,
+}
+const myChart = new Chart(
+    document.getElementById('mychart').getContext("2d"),
+    config
+);
